c94edfa20e7119241e71700f082801cb2092e292
Ameer67 <ameerzaita9@gmail.com>
2023-01-16 22:23:26 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ let pendants = document.querySelectorAll("#pendant");
 const pendantInfo = document.getElementById("pendant-info");
 let pendantSize;
 let emptySpaceColor;
-let bgRepeat;
+// let bgRepeat;
 
 pendantInfo.addEventListener("change", function(event) {
     pendantSize = parseInt(document.getElementById("pendant-size").value);
     emptySpaceColor = document.getElementById("empty-space-color").value;
-    bgRepeat = document.getElementById("bg-repeat").checked;
+    // bgRepeat = document.getElementById("bg-repeat").checked;
     for (i = 0; i < pendants.length; i++) {
         pendants[i].style.height = pendantSize + 'mm';
         pendants[i].style.width = pendantSize + 'mm';
         pendants[i].style.backgroundColor = emptySpaceColor;
 
-        if (bgRepeat === true) {
-            pendants[i].style.backgroundRepeat = 'repeat';
-        } else {
-            pendants[i].style.backgroundRepeat = 'no-repeat';
-        }
+        // if (bgRepeat === true) {
+        //     pendants[i].style.backgroundRepeat = 'repeat';
+        // } else {
+        //     pendants[i].style.backgroundRepeat = 'no-repeat';
+        // }
     }    
 })
 
