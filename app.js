a2cf1924bf6cd40a6c73d74a970567502ab80767
Ameer67 <ameerzaita9@gmail.com>
2023-01-17 19:23:33 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,47 @@
 let pendants = document.querySelectorAll("#pendant");
 
 const pendantInfo = document.getElementById("pendant-info");
-let pendantSize;
-let emptySpaceColor;
-// let bgRepeat;
-
+let pendantSize, emptySpaceColor;
 pendantInfo.addEventListener("change", function(event) {
     pendantSize = parseInt(document.getElementById("pendant-size").value);
     emptySpaceColor = document.getElementById("empty-space-color").value;
-    // bgRepeat = document.getElementById("bg-repeat").checked;
     for (i = 0; i < pendants.length; i++) {
         pendants[i].style.height = pendantSize + 'mm';
         pendants[i].style.width = pendantSize + 'mm';
         pendants[i].style.backgroundColor = emptySpaceColor;
-
-        // if (bgRepeat === true) {
-        //     pendants[i].style.backgroundRepeat = 'repeat';
-        // } else {
-        //     pendants[i].style.backgroundRepeat = 'no-repeat';
-        // }
     }    
 })
 
 const uploadBtn = document.getElementById('customer-photo-upload');
-let customerPhotoURL;
-
 uploadBtn.addEventListener("change", function(event) {
-    customerPhotoURL = URL.createObjectURL(event.target.files[0]);
     for (i = 0; i < pendants.length; i++) {
-        pendants[i].style.backgroundImage = "url(" + customerPhotoURL + ")";
+        pendants[i].style.backgroundImage = "url(" + URL.createObjectURL(event.target.files[0]) + ")";
     }
 });
 
-// let copiesSelect = document.getElementById("copies");
-// copiesSelect.addEventListener("change", function(event){
-//     const copies = parseInt(copiesSelect.value);
-
-//     const pendantDiv = document.createElement("div");
-//     pendantDiv.id = "pendant;"
-    
-//     if (copies > 1) {
-//         // check how many pendants already exist
-//     }
-
-//     document.body.appendChild(pendantDiv);
-// });
+let scale, positionUpDown, positionLeftRight, rotate;
+let brightness = document.getElementById('brightness').value;
 
 const photoEditor = document.getElementById('photo-editor');
-
 photoEditor.addEventListener("input", function(event) {
-    let scale = document.getElementById('scale').value;
-    let positionUpDown = document.getElementById('position-up-down').value;
-    let positionLeftRight = document.getElementById('position-left-right').value;
-    let rotate = document.getElementById('rotate').value;
-    let brightness = document.getElementById('brightness').value;
+    scale = document.getElementById('scale').value;
+    positionUpDown = document.getElementById('position-up-down').value;
+    positionLeftRight = document.getElementById('position-left-right').value;
+    rotate = document.getElementById('rotate').value;
+    brightness = document.getElementById('brightness').value;
+    pendantPreviewChanges(scale, positionUpDown, positionLeftRight, rotate, brightness);
 
+    changeBrightnessValueText();
+})
+
+const brightnessValueText = document.getElementById('brightness-value-text');
+function changeBrightnessValueText() {
+    brightnessValueText.innerText = brightness;
+}
+
+changeBrightnessValueText();
+
+function pendantPreviewChanges(scale, positionUpDown, positionLeftRight, rotate, brightness) {
     for (i = 0; i < pendants.length; i++) {
         pendants[i].style.backgroundSize = scale + '%';
         pendants[i].style.backgroundPositionX = positionLeftRight + '%';
@@ -62,6 +49,4 @@ photoEditor.addEventListener("input", function(event) {
         pendants[i].style.transform = 'rotate(' + rotate + 'deg)';
         pendants[i].style.filter = 'brightness(' + brightness + ')';
     }
-    
-})
-
+}
